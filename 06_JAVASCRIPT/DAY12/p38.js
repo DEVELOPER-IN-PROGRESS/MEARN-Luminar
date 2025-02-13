0fb9ea525b2cf4d8851a74b15706c40596c61c84
iceweasel <johnsjosephk@pm.me>
2025-02-13 13:52:02 +0530

    js pattern generators complete

---
+++ 06_JAVASCRIPT/DAY12/p38.js
@@ -0,0 +1,15 @@
+//w.a. function to display 'javscript' on every call
+
+function display(){
+    console.log('Javascript')
+}
+
+display()
+
+//w.a.f to find sum of two numbers using function
+
+function sum(x,y){
+    console.log(x+y)
+}
+
+sum(9,8)
---
+++ 06_JAVASCRIPT/DAY12/pr39.js
@@ -0,0 +1,23 @@
+// w.a.f to find a given number is odd or even
+
+function is_odd(a){
+    console.log(a%2==0?`Given number is odd`:`Given Number is Even`)
+}
+
+is_odd(10);
+
+// w.a.f to find a given number is positive or negative
+
+function is_positive(num){
+    console.log(num>0?`Given Number is positive`:num<0?`Number is negative`:`Number 0`);
+}
+
+is_positive(0)
+
+// w.a.f to find cube of a given number
+
+function find_cube(n){
+    console.log(`Cube of ${n} is ${n**3}`);
+}
+
+find_cube(9)
