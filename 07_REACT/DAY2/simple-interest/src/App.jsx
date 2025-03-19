2242c2c660bf1bdde0c2a7b1692eccb8cdfcb30a
iceweasel <johnsjosephk@pm.me>
2025-03-19 14:32:10 +0530

    simple interest calc complete

--- 07_REACT/DAY2/simple-interest/src/App.jsx
+++ 07_REACT/DAY2/simple-interest/src/App.jsx
@@ -1,10 +1,92 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let calc_amount = 0
+  const [ pnr , setPnr ]  = useState({
+      principal:"",
+      rate:"",
+      year:"",
+  })
+
+  const [isPrincipal, setIsPrincipal] = useState(true);
+  const [isRate, setIsRate] = useState(true);
+  const [isYear, setIsYear] = useState(true);
+
+  const [ interest , setInterest] =  useState("");
+
+  const validate = (e) =>{
+      // console.log(e)
+
+      const { name , value } = e.target;
+
+      console.log(value)
+
+      /*
+      if( isNaN(Number(value))){
+          return
+      }
+      */
+      let reg = '^[0-9]*$'
+
+      // console.log(value, value.match(reg) )
+
+      /*
+        if(value.match(reg)){
+            setPnr({...pnr, [name]:value })
+        }
+      */
+
+      //invalid input check
+      if(value.match(reg)){
+
+        switch(name){
+          case 'principal' :
+                         setIsPrincipal(true)
+                         break
+          case 'rate' : setIsRate(true)
+                         break
+          case 'year':  setIsYear(true)
+                        break
+        }
+        setPnr({...pnr, [name]:value })
+
+      }else{
+        switch(name){
+          case 'principal' :
+                         setIsPrincipal(false)
+                         break
+          case 'rate' : setIsRate(false)
+                         break
+          case 'year':  setIsYear(false)
+                        break
+      }
+      setPnr({...pnr, [name]:value })
+    }
+  }
+
+  const handleCalculate = () => {
+     let simpleInterest =  (pnr.principal*pnr.year*pnr.rate)/100 ;
+
+     if(isNaN(simpleInterest))
+        return
+
+      setInterest(simpleInterest);
+  }
+
+  const handleReset = () =>{
+    setPnr({ principal:"",
+      rate:"",
+      year:"",})
+
+      setIsPrincipal(true)
+      setIsRate(true)
+      setIsYear(true)
+      setInterest("");
+  }
+
+  console.log(pnr)
 
   return (
     <>
@@ -13,22 +95,44 @@ function App() {
               <h1 className="heading">Simple Interest</h1>
               <p className="desc">Calculate your simple interst</p>
               <div className="box">
-                  <p className="money">₹ <span>{calc_amount}</span></p>
-                  <p className="desc">total simple interst</p>
+                  <p className="money">₹ <span>{ interest }</span></p>
+                  <p className="desc">total simple interst {}</p>
               </div>
 
+              <div className="mb-3">
+                <TextField value={pnr.principal} id="outlined-basic" name="principal" onChange={(e) => { validate(e) }} className="w-100" label="Principal Amount" variant="outlined" />
+                   { !isPrincipal && <span className="text-danger">Invalid Input</span> }
+              </div>
+              <div className="mb-3">
+                <TextField value={pnr.rate}  id="outlined-basic" name="rate" onChange={(e) => { validate(e) }} className="w-100" label="Rate of Interest(%)" variant="outlined" />
+                  { !isRate && <span className="text-danger">Invalid Input</span> }
+              </div>
+              <div className="mb-3">
+                <TextField value={pnr.year}  id="outlined-basic" name="year" onChange={(e) => { validate(e) }} className="w-100" label="Year (Yr)" variant="outlined" />
+                  {  !isYear && <span className="text-danger">Invalid Input</span> }
+              </div>
+              <div className="mb-3"></div>
+
+              <div className="d-flex align-items-center justify-content-center gap-3">
+                 <Button variant="contained" onClick={ () => { handleCalculate() }} className="p-3 w-50" color="success">
+                    Calculate
+                </Button>
+                <Button variant="outlined" onClick={ () => { handleReset() }} className="p-3 w-50">
+                    Reset
+                </Button>
+                 </div>
+
+              {/*
               <form action="" className="calculator">
                  <input type="number" placeholder='₹ Principle amount()' id="principal" className="amount_box" onChange = { (e) => {}}/>
 
-                 <input type="number"  placeholder='Rate of Interest(%)'  id="" className="amount_box" onChange = { (e) => {}}/>
+                 <input type="number"  placeholder=''  id="" className="amount_box" onChange = { (e) => {}}/>
 
                  <input type="number" placeholder='Year (Yr)'  id="" className="amount_box" onChange = { (e) => {}}/>
 
-                 <div className="button-wrap">
-                   <button className="btn calculate">calculate</button>
-                   <button className="btn reset">reset</button>
-                 </div>
+
               </form>
+              */}
           </div>
       </div>
 
