e2b0a0beab6fb90c9567627a7bc47e90a7f9a814
iceweasel <johnsjosephk@pm.me>
2025-03-20 13:30:24 +0530

    media player frontend theme loading , React Routes'

--- 07_REACT/DAY2/media-player/frontend/src/main.jsx
+++ 07_REACT/DAY2/media-player/frontend/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+// Bootstrap css  use the below import if you are not using the theme
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// bootswatch theme
+import './bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+  </BrowserRouter>
 )
