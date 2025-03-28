d01e385e06efda8a9b090a01b4bd53d642896210
iceweasel <johnsjosephk@pm.me>
2025-03-28 13:35:20 +0530

    useEffect , iframe embed link issue fixed

--- 07_REACT/DAY2/media-player/frontend/src/components/Videocard.jsx
+++ 07_REACT/DAY2/media-player/frontend/src/components/Videocard.jsx
@@ -3,20 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 
-function Videocard() {
+function Videocard({video}) {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
   return (
     <>
-    <Card className="mt-3 mt-lg-2" style={{ }}>
-      <Card.Img  onClick={handleShow} variant="top" src="https://preview.redd.it/23fn7ji6t96d1.png?width=640&crop=smart&auto=webp&s=1911ba13a7fca17eaf18954c2f3ea8f9165fb879" />
+    <Card className="mt-3 mt-lg-2" style={{}}>
+      <Card.Img  onClick={handleShow} variant="top" className='img-fluid' style={{height:'250px'}} src={`${video?.image}`} />
       <Card.Body className='d-flex align-items-center justify-content-between'>
-        <Card.Text  >Card Text</Card.Text>
+        <Card.Text>{ video?.caption }</Card.Text>
         <Button variant="danger">
             <FontAwesomeIcon icon={faTrashCan} />
         </Button>
@@ -25,10 +25,10 @@ function Videocard() {
 
     <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{video?.caption}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <iframe width="100%" height="300" src="https://www.youtube.com/embed/6LD30ChPsSs?autoplay=1" title="The Life of Ram Video Song | 96 | Vijay Sethupathi, Trisha | Govind Vasantha | C. Prem Kumar" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
+          <iframe width="100%" height="300" src={`${video?.embedLink}?autoplay=1`} title={`${video?.caption}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
           </iframe>
         </Modal.Body>
       </Modal>
--- 07_REACT/DAY2/media-player/frontend/src/components/View.jsx
+++ 07_REACT/DAY2/media-player/frontend/src/components/View.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Videocard from './Videocard';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { allVideosApi } from '../services/allApi';
 
 function View() {
+
+  const [allVideo , setAllVideo ] = useState([])
+
+  const getAllVideo = async () => {
+	const result = await allVideosApi();
+	if(result.status >=200 && result.status <300){
+		setAllVideo(result.data)
+	}
+  }
+
+  useEffect(()=>{
+	getAllVideo()
+  })
+
+// console.log(allVideo)
+
   return (
     <>
         <h5>All Videos</h5>
         <div className="container-fluid mt-5">
             <Row className='justify-content-start column-gap-2 ps-lg-2 '>
-                <Col sm={12} md={3} className="" lg={3}>
-                        <Videocard/>
-                </Col>
-                <Col sm={12} md={3} className="" lg={3}>
-                        <Videocard/>
-                </Col>
-                <Col sm={12} md={3} className="" lg={3}>
-                        <Videocard/>
-                </Col>
-                <Col sm={12} md={3} className="" lg={3}>
-                        <Videocard/>
-                </Col>
+		{
+		allVideo?.length > 0?
+
+                 allVideo.map( (video,idx) => (
+                <Col key={`col-${idx}`} sm={12} md={3} className="" lg={3}>
+                        <Videocard key={`card-${idx}`} video={video}/>
+                </Col>))
+		:
+		<Col className="d-flex justify-content-center align-items-center flex-column">
+		   <img src="" alt="no videos uploaded"/>
+		</Col>
+                }
             </Row>
         </div>
 
