92b5e03152fcc63e27eb4a400fca1825fb5ff8eb
iceweasel <johnsjosephk@pm.me>
2025-05-15 20:23:59 +0530

    bookstore frontend notes 15/05/2025

--- 08_NODE/DAY1/backend/databaseconnection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const connectionString = process.env.DATABASE;
-mongoose.connect(connectionString).then( () => { console.log('mongoose connected')})
-.catch( (err) => { console.log(`mongodb connection failed due to ${err}`) })
-
-
